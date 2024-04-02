01a04aa2006a63023b12455660497898e50e0aa6
Debapriya Das <debapriyadas73@gmail.com>
2024-04-03 03:50:46 +0530

    dotenv setup for db

--- database/connection.js
+++ database/connection.js
@@ -1,11 +1,10 @@
 const db = require("mysql2");
+require("dotenv").config();
 
 var con = db.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "1234",
-  database: "backend_assignment",
-  //   port: "3306",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 });
 
 con.connect(function (err) {
